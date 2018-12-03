ea331589233d65d59cc48b97476813e16dcf8e74
Roch <rochfolly@hotmail.fr>
2018-12-03 15:34:57 +0100

    front

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
 
 class App extends Component {
 
@@ -9,7 +15,7 @@ class App extends Component {
     super(props);
     this.state={
       data:"",
-      urlrecipe:"http://51.75.249.160:3001/",
+      urlrecipe:"http://51.75.249.160:3001/en/",
       title:"",
       teaser:"",
       type:"",
@@ -21,6 +27,24 @@ class App extends Component {
     }
     this.getrecipe = this.getrecipe.bind(this);
   }
+
+  /*const styles = theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+    position: 'relative',
+    overflow: 'auto',
+    maxHeight: 300,
+  },
+  listSection: {
+    backgroundColor: 'inherit',
+  },
+  ul: {
+    backgroundColor: 'inherit',
+    padding: 0,
+  },
+}); */
 
   getrecipe(){
     fetch(this.state.urlrecipe)
@@ -38,6 +62,21 @@ class App extends Component {
       );
   }
 
+  renderRecipe(){
+   return(
+     <div className="recipe">
+       <h2>{this.state.data.title}</h2>
+       <h3>{this.state.data.teaser}</h3>
+       <p>Pays d'origine: {this.state.data.cuisine}</p>
+       <ul>Ingrédients: {this.state.data.ingredients.map((ingredient) =>
+       <li></li>)}</ul>
+       <p> {this.state.data.country}</p>
+       <p>Temps de préparation: {this.state.data.time}</p>
+     </div>
+   )
+ }
+
+
   render() {
     return (
       <div className="App">
@@ -47,6 +86,7 @@ class App extends Component {
           </Button>
 
           <p>{this.state.data.title}</p>
+          {this.renderRecipe()}
 
       </div>
     );
