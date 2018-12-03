06e6a89eebca987863e3f2ede6e8396fd983c3e6
alberic dufort <alberic.dufort@gmail.com>
2018-12-03 15:57:19 +0100

    list ingredients

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -49,26 +49,16 @@ class App extends Component {
   getrecipe(){
     fetch(this.state.urlrecipe)
       .then(response => response.json())
-      .then(data =>
-        this.setState({ data:data }),
-        this.getingredients()
-        /*,
-        this.setState({ title:this.state.data.title }),
-        this.setState({ teaser:this.state.data.teaser }),
-        this.setState({ type:this.state.data.type }),
-        this.setState({ cheff:this.state.data.cheff }),
-        this.setState({ photographer:this.state.data.photographer }),
-        this.setState({ country:this.state.data.country }),
-        this.setState({ time:this.state.data.time }),
-        this.setState({ ingredients:this.state.data.ingredients })*/
+      .then(data =>{
+        this.setState({ data:data })
+        this.getingredients(data.ingredients)}
       );
   }
 
-
-  getingredients(){
-    var listItems = this.state.data.ingredients;
-    const myArrStr = JSON.stringify(listItems);
-    this.setState({ ingredients:myArrStr })
+  getingredients(ingredients){
+    var listItems = ingredients;
+    //var myArrStr = JSON.parse(listItems);
+    this.setState({ ingredients:listItems })
   }
 
   renderRecipe(){
@@ -77,12 +67,21 @@ class App extends Component {
        <h2>{this.state.data.title}</h2>
        <h3>{this.state.data.teaser}</h3>
        <p>Pays d''origine: {this.state.data.cuisine}</p>
-       <ul>Ingrédients: {this.state.data.ingredients.map((ingredient) =>
-       <li></li>)}</ul>
+       {this.renderIngredients()}
        <p> {this.state.data.country}</p>
        <p>Temps de préparation: {this.state.data.time}</p>
      </div>
    )
+ }
+
+ renderIngredients(){
+   if(typeof this.state.ingredients != 'undefined')
+    {
+      let ingredient =  this.state.ingredients.map(x=>
+        <p>{x}</p>
+      )
+       return ingredient;
+    }
  }
 
   render() {
@@ -92,15 +91,13 @@ class App extends Component {
           <Button onClick={() => this.getrecipe()}>
             Fetch recipe
           </Button>
-
           <p>{this.state.data.title}</p>
           {this.renderRecipe()}
-
       </div>
     );
   }
 }
-/*          <p>cuisine:{this.state.data.cuisine}</p>
+/*<p>cuisine:{this.state.data.cuisine}</p>
           <p>ingrédients:{this.state.data.ingredients}</p>
           {typeof this.state.data.ingredients == 'undefined'?
           <p></p>:<p>{this.state.data.ingredients.length}</p>}*/
